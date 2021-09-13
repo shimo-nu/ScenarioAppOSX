29716f3c344088a6c4a0a9d885736672f0c0397a
shimonori <8index103000@gmail.com>
2021-09-14 02:38:31 +0900

    first commit

---
+++ ScenarioOSX/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScenarioOSX
+//
+//  Created by 下里浩昇 on 2021/09/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
